e624815c74a15b52206485fc43d7a9dad3268955
Abhinandan Khurana <75041733+Abhinandan-Khurana@users.noreply.github.com>
2023-11-30 15:24:11 +0530

    Created finder.sh

---
+++ finder.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "FINDER:~"
+read -p "Enter the file name you want to find: " file_name
+
+find / -type f -name "$file_name" 2>/dev/null & pid=$! # Process Id of the previous running command
+
+spin='-\|/'
+
+i=0
+while kill -0 $pid 2>/dev/null
+do
+  i=$(( (i+1) %4 ))
+  printf "\r${spin:$i:1}"
+  sleep .1
+done
