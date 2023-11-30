76253de46092fb7ce4d9461bc0ae346e4b9f10d9
Abhinandan Khurana <75041733+Abhinandan-Khurana@users.noreply.github.com>
2023-11-30 15:48:53 +0530

    Create spinner.sh

---
+++ spinner.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sleep 10 & pid=$! # Process Id of the previous running command
+
+spin='-\|/'
+
+i=0
+while kill -0 $pid 2>/dev/null
+do
+  i=$(( (i+1) %4 ))
+  printf "\r${spin:$i:1}"
+  sleep .1
+done
